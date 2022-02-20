eab20cac5fd5533f47d68046f8969759bd67e0da
mika1407 <koneautomaatio@hotmail.com>
2022-02-20 10:44:56 +0200

    eka commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row min-vh-100">
+        <div className="col d-flex flex-column justify-content-center align-items-center">
+          <h1>Hello Bootstrap!</h1>
+
+          {renderPostsTable()}
+        </div>
+      </div>      
+    </div>
+  );
+}
+
+function renderPostsTable(){
+  return (
+    <div className="table-responsive mt-5">
+      <table className="table table-bordered border-dark">
+        <thead>
+          <tr>
+            <th scope="col">PostId (PK)</th>
+            <th scope="col">Title</th>
+            <th scope="col">Content</th>
+            <th scope="col">CRUD Operations</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Post 1 title</td>
+            <td>Post 1 content</td>
+            <td>
+              <button className="btn btn-dark btn-lg mx-3 my-3">Update</button>
+              <button className="btn btn-secondary btn-lg">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default App;

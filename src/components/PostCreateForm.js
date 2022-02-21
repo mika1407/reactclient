eb9803e2b1d419182f6f01ddab89271c8801dbb6
mika1407 <koneautomaatio@hotmail.com>
2022-02-21 14:11:49 +0200

    nyt toimii post

--- src/components/PostCreateForm.js
+++ src/components/PostCreateForm.js
@@ -28,7 +28,7 @@ export default function PostCreateForm(props) {
         const url = Constants.API_URL_CREATE_POST;
 
         fetch(url, {
-            methot: 'POST',
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
@@ -47,17 +47,17 @@ export default function PostCreateForm(props) {
     };
 
     return (
-        <form className='w-100 px-5'>
-            <h1 className='mt-5'>Create new post</h1>
+        <form className="w-100 px-5">
+            <h1 className="mt-5">Create new post</h1>
 
-            <div className='mt-5'>
-                <label className='h3 form-label'>Post title</label>
-                <input value={formData.title} name="title" type="text" className='form-control' onChange={handleChange} />
+            <div className="mt-5">
+                <label className="h3 form-label">Post title</label>
+                <input value={formData.title} name="title" type="text" className="form-control" onChange={handleChange} />
             </div>
 
-            <div className='mt-4'>
-                <label className='h3 form-label'>Post content</label>
-                <input value={formData.content} name="content" type="text" className='form-control' onChange={handleChange} />
+            <div className="mt-4">
+                <label className="h3 form-label">Post content</label>
+                <input value={formData.content} name="content" type="text" className="form-control" onChange={handleChange} />
             </div>
 
             <button onClick={handleSubmit} className="btn btn-dark btn-lg w-100 mt-5">Submit</button>
